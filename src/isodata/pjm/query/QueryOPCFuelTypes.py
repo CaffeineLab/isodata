13d4f1aa8c50d45f45b6b006691e1f9ae2284cdf
GR <glenn@caffeinelab.com>
2023-10-25 16:03:15 -0700

    add even some more queries (OPC)

---
+++ src/isodata/pjm/query/QueryOPCFuelTypes.py
@@ -0,0 +1,7 @@
+"""QueryOPCFuelTypes - Undocumented"""
+# pylint:disable=duplicate-code
+
+
+def prepare(token, **kwargs):
+    """prepare and return all the components of the requests call."""
+    return None
---
+++ src/isodata/pjm/query/QueryOPCOpportunityCosts.py
@@ -0,0 +1,8 @@
+"""QueryOPCOpportunityCosts -
+"""
+# pylint:disable=duplicate-code
+
+
+def prepare(token, **kwargs):
+    """prepare and return all the components of the requests call."""
+    return None
